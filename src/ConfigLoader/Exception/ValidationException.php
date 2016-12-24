25ce9ccc80fe6ba57d7f8e931952b410bcf7f27a
Ilija Studen <ilija.studen@activecollab.com>
2016-12-24 15:09:13 +0100

    Improve config validation exception message

--- src/ConfigLoader/Exception/ValidationException.php
+++ src/ConfigLoader/Exception/ValidationException.php
@@ -28,14 +28,20 @@ class ValidationException extends RuntimeException
 
     public function &missing($option_name)
     {
-        $this->missing_options[] = $option_name;
+        if (!in_array($option_name, $this->missing_options)) {
+            $this->missing_options[] = $option_name;
+            $this->rebuildExceptionMessage();
+        }
 
         return $this;
     }
 
     public function &missingValue($option_name)
     {
-        $this->missing_option_values[] = $option_name;
+        if (!in_array($option_name, $this->missing_option_values)) {
+            $this->missing_option_values[] = $option_name;
+            $this->rebuildExceptionMessage();
+        }
 
         return $this;
     }
@@ -43,5 +49,18 @@ class ValidationException extends RuntimeException
     public function hasErrors()
     {
         return !empty($this->missing_options) || !empty($this->missing_option_values);
+    }
+
+    private function rebuildExceptionMessage()
+    {
+        $this->message = 'Found config options do not match configured requirements.';
+
+        if (!empty($this->missing_options)) {
+            $this->message .= ' Options not present: ' . implode(', ', $this->missing_options) . '.';
+        }
+
+        if (!empty($this->missing_option_values)) {
+            $this->message .= ' Options not present, or empty: ' . implode(', ', $this->missing_option_values) . '.';
+        }
     }
 }
