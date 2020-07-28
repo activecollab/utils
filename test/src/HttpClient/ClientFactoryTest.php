9fae40a70142b070062bf3d18078811354e521f4
Ilija Studen <ilija.studen@activecollab.com>
2020-07-28 07:52:51 +0200

    Test if Client factory executes client middlewares when creating a client

---
+++ test/src/HttpClient/ClientFactoryTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Utils project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Utils\Test\HttpClient;
+
+use ActiveCollab\HttpClient\Client\ClientFactory;
+use ActiveCollab\HttpClient\Client\ClientFactoryInterface;
+use ActiveCollab\HttpClient\Client\ClientInterface;
+use ActiveCollab\HttpClient\Configure\ClientMiddleware\ClientMiddlewareInterface;
+use ActiveCollab\Utils\Test\Base\TestCase;
+use Laminas\Diactoros\ResponseFactory;
+use Psr\Http\Message\ResponseFactoryInterface;
+
+class ClientFactoryTest extends TestCase
+{
+    public function testWillUseDefaultClientMiddlewares(): void
+    {
+        $clientMock = $this->createMock(ClientInterface::class);
+        $middleware1 = $this->createMock(ClientMiddlewareInterface::class);
+        $middleware1
+            ->expects($this->once())
+            ->method('alter');
+
+        $middleware2 = $this->createMock(ClientMiddlewareInterface::class);
+        $middleware2
+            ->expects($this->once())
+            ->method('alter');
+
+        $clientFactory = $this->createTestFactory($clientMock, $middleware1, $middleware2);
+
+        $clientFactory->createClient();
+    }
+
+    public function testWillUseRequestClientMiddlewares(): void
+    {
+        $this->fail('No implemented');
+    }
+
+    private function createTestFactory(ClientInterface $client, ClientMiddlewareInterface ...$defaultMiddlewares): ClientFactoryInterface
+    {
+        return new class ($client, ...$defaultMiddlewares) extends ClientFactory
+        {
+            private ClientInterface $client;
+
+            public function __construct(
+                ClientInterface $client,
+                ClientMiddlewareInterface ...$defaultMiddlewares
+            )
+            {
+                parent::__construct(new ResponseFactory(), ...$defaultMiddlewares);
+
+                $this->client = $client;
+            }
+
+            protected function doCreateClient(): ClientInterface
+            {
+                return $this->client;
+            }
+        };
+    }
+}
