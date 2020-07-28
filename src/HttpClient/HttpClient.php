7b2bd7cf1a29d9f22f7c6162dbf0b200d4edb5ff
Ilija Studen <ilija.studen@activecollab.com>
2020-07-28 08:33:27 +0200

    Use client factory in HttpClient

--- src/HttpClient/HttpClient.php
+++ src/HttpClient/HttpClient.php
@@ -10,9 +10,11 @@ declare(strict_types=1);
 
 namespace ActiveCollab\HttpClient;
 
+use ActiveCollab\HttpClient\Client\ClientInterface;
+use ActiveCollab\HttpClient\Client\Factory\ClientFactoryInterface;
+use ActiveCollab\HttpClient\Configure\ClientMiddleware\ClientMiddlewareInterface;
 use ActiveCollab\HttpClient\Configure\ConfigureMiddlewareInterface;
 use ActiveCollab\HttpClient\Configure\RequestMiddleware\RequestMiddlewareInterface;
-use Psr\Http\Client\ClientInterface as PsrHttpClient;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -25,48 +27,61 @@ class HttpClient implements HttpClientInterface
     const METHOD_PATCH = 'PATCH';
     const METHOD_DELETE = 'DELETE';
 
-    private PsrHttpClient $httpClient;
+    private ClientFactoryInterface $clientFactory;
     private RequestFactoryInterface $requestFactory;
 
-    public function __construct(PsrHttpClient $httpClient, RequestFactoryInterface $requestFactory)
+    public function __construct(ClientFactoryInterface $clientFactory, RequestFactoryInterface $requestFactory)
     {
-        $this->httpClient = $httpClient;
+        $this->clientFactory = $clientFactory;
         $this->requestFactory = $requestFactory;
     }
 
     public function get(string $url, ConfigureMiddlewareInterface ...$configureMiddlewares): ResponseInterface
     {
-        return $this->httpClient->sendRequest(
+        return $this->prepareClient($configureMiddlewares)->sendRequest(
             $this->prepareRequest(self::METHOD_GET, $url, ...$configureMiddlewares)
         );
     }
 
     public function post(string $url, ConfigureMiddlewareInterface ...$configureMiddlewares): ResponseInterface
     {
-        return $this->httpClient->sendRequest(
+        return $this->prepareClient($configureMiddlewares)->sendRequest(
             $this->prepareRequest(self::METHOD_POST, $url, ...$configureMiddlewares)
         );
     }
 
     public function put(string $url, ConfigureMiddlewareInterface ...$configureMiddlewares): ResponseInterface
     {
-        return $this->httpClient->sendRequest(
+        return $this->prepareClient($configureMiddlewares)->sendRequest(
             $this->prepareRequest(self::METHOD_PUT, $url, ...$configureMiddlewares)
         );
     }
 
     public function patch(string $url, ConfigureMiddlewareInterface ...$configureMiddlewares): ResponseInterface
     {
-        return $this->httpClient->sendRequest(
+        return $this->prepareClient($configureMiddlewares)->sendRequest(
             $this->prepareRequest(self::METHOD_PATCH, $url, ...$configureMiddlewares)
         );
     }
 
     public function delete(string $url, ConfigureMiddlewareInterface ...$configureMiddlewares): ResponseInterface
     {
-        return $this->httpClient->sendRequest(
+        return $this->prepareClient($configureMiddlewares)->sendRequest(
             $this->prepareRequest(self::METHOD_DELETE, $url, ...$configureMiddlewares)
         );
+    }
+
+    private function prepareClient(ConfigureMiddlewareInterface ...$configureMiddlewares): ClientInterface
+    {
+        $client = $this->clientFactory->createClient();
+
+        foreach ($configureMiddlewares as $configureMiddleware) {
+            if ($configureMiddleware instanceof ClientMiddlewareInterface) {
+                $client = $configureMiddleware->alter($client);
+            }
+        }
+
+        return $client;
     }
 
     private function prepareRequest(
